a634a5583fd4d4b51fba0f8064def0a3b8597e51
Charles A Edwards <charles.a.edwards@btinternet.com>
2016-02-13 15:50:49 +0000

    Altered the logic
    Altered the logic so that it will not execute code that should not be
    executed if a failure occurs.

--- DBC_Extractor/dbc_extractor.cpp
+++ DBC_Extractor/dbc_extractor.cpp
@@ -30,24 +30,29 @@ int main(int argc, char* arg[])
         if (!SFileOpenArchive("dbc.MPQ", 0, 0, &hMPQArchiveFile))
             std::cout << "Balls, cannot locate the MPQ file!!!" << std::endl << std::endl; 
         else
+        {
             std::cout << "Yay, located the the MPQ file!!!" << std::endl << std::endl;
 
-        // go through the archive and list each file
-        SFILE_FIND_DATA pFile;
-        if ((hDBCFile = SFileFindFirstFile(hMPQArchiveFile, "*.dbc", &pFile, 0)) != NULL)
-        {
-            std::cout << "File: " << pFile.cFileName << std::endl;
-            std::cout << "=============" << std::endl;
-
-            // grab the rest of the dbc files
-            while (SFileFindNextFile(hDBCFile, &pFile) != NULL)
+            // go through the archive and list each file
+            SFILE_FIND_DATA pFile;
+            if ((hDBCFile = SFileFindFirstFile(hMPQArchiveFile, "*.dbc", &pFile, 0)) != NULL)
             {
                 std::cout << "File: " << pFile.cFileName << std::endl;
+                std::cout << "=============" << std::endl;
+
+                // grab the rest of the dbc files
+                while (SFileFindNextFile(hDBCFile, &pFile) != NULL)
+                {
+                    std::cout << "File: " << pFile.cFileName << std::endl;
+                
+                    // write dbc file to disk
+
+                }
             }
-        }
-        else
-        {
-            std::cout << "PROBLEM! Archive is empty!!! " << std::endl;
+            else
+            {
+                std::cout << "PROBLEM! Archive is empty!!! " << std::endl;
+            }
         }
     }
     else // failed to locate the WoW executable
