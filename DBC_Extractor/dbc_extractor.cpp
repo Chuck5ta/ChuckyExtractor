af0e9e2c00178ebb7bdecc70619f33247bca768b
Charles A Edwards <charles.a.edwards@btinternet.com>
2016-02-15 06:54:01 +0000

    File extraction moved to its own function
    File extraction moved to its own function.

--- DBC_Extractor/dbc_extractor.cpp
+++ DBC_Extractor/dbc_extractor.cpp
@@ -12,6 +12,34 @@
 
 
 /*
+ * this function extracts a single DBC file from the dbc.MPQ archive and writes it to the disk
+ *
+ * @PARAM HANDLE hCurrentDBCFile;    // handle to the archived dbc file currently being processed
+ * @PARAM HANDLE hDiskFile;          // handle to the dbc file that will be created on disk
+ * @PARAM SFILE_FIND_DATA pFile;     // a pointer to the current dbc file in the dbc.MPQ archive
+ *
+ */
+void extractDBCFile(HANDLE hCurrentDBCFile, HANDLE hDiskFile, SFILE_FIND_DATA pFile)
+{
+    char  szBuffer[0x10000];
+    DWORD dwBytes = 1;
+
+    // extract the file and write it to the disk
+    while (dwBytes > 0)
+    {
+        // extract the dbc file's contents from the MPQ archive
+        SFileReadFile(hCurrentDBCFile, szBuffer, sizeof(szBuffer), &dwBytes, NULL);
+        if (dwBytes > 0)
+            // write the contents to the new file on disk
+            if (WriteFile(hDiskFile, szBuffer, dwBytes, &dwBytes, NULL))
+                std::cout << "File extraction successful, creating file:  " << pFile.cFileName << std::endl;
+            else
+                std::cout << "Failed to write the file to the disk:  " << pFile.cFileName << std::endl;
+    }
+}
+
+
+/*
  * This function extracts the DBC files from the dbc.MPQ archive and writes them to the disk as individual files.
  */
 void processTheDBCMPQFile()
@@ -46,24 +74,10 @@ void processTheDBCMPQFile()
                 //  create the file on disk, so that we can write to it
                 if (hDiskFile = CreateFile(pFile.szPlainName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL))
                 {
-                    std::cout << "Successfully created disk file handle:  " << pFile.szPlainName << std::endl;
-                    // write dbc file to disk
+                    std::cout << "Successfully created the disk file's handle:  " << pFile.szPlainName << std::endl;
 
-                    char  szBuffer[0x10000];
-                    DWORD dwBytes = 1;
-
-                    // extract the file and write it to disk
-                    while (dwBytes > 0)
-                    {
-                        // extract the dbc file's contents from the MPQ archive
-                        SFileReadFile(hDBCNextFile, szBuffer, sizeof(szBuffer), &dwBytes, NULL);
-                        if (dwBytes > 0)
-                            // write the contents to the new file on disk
-                            if (WriteFile(hDiskFile, szBuffer, dwBytes, &dwBytes, NULL))
-                                std::cout << "File extraction successful, creating file:  " << pFile.cFileName << std::endl;
-                            else
-                                std::cout << "Failed to write file to disk:  " << pFile.cFileName << std::endl;
-                    }
+                    // extract and write dbc file to disk
+                    extractDBCFile(hDBCNextFile, hDiskFile, pFile);
 
                     // grab the rest of the dbc files
                     // ==============================
@@ -75,42 +89,27 @@ void processTheDBCMPQFile()
                         // open the dbc file in the MPQ archive
                         if (SFileOpenFileEx(hMPQArchiveFile, pFile.cFileName, 0, &hDBCNextFile))
                         {
-                            std::cout << "Successfully openned file:  " << pFile.cFileName << std::endl;
+                            std::cout << "Successfully openned the file:  " << pFile.cFileName << std::endl;
                             // create the file on disk, so that we can write to it
                             if (hDiskFile = CreateFile(pFile.szPlainName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL))
                             {
-                                std::cout << "Successfully created disk file handle:  " << pFile.cFileName << std::endl;
+                                std::cout << "Successfully created the disk file's handle:  " << pFile.cFileName << std::endl;
 
-                                char  szBuffer[0x10000];
-                                DWORD dwBytes = 1;
-
-                                // extract the dbc file and write it to disk
-                                while (dwBytes > 0)
-                                {
-                                    // extract the dbc file's contents from the MPQ archive
-                                    SFileReadFile(hDBCNextFile, szBuffer, sizeof(szBuffer), &dwBytes, NULL);
-                                    if (dwBytes > 0)
-                                    {
-                                        // write the contents to the new file on disk
-                                        if (WriteFile(hDiskFile, szBuffer, dwBytes, &dwBytes, NULL))
-                                            std::cout << "File extraction successful, creating file:  " << pFile.cFileName << std::endl;
-                                        else
-                                            std::cout << "Failed to write file to disk:  " << pFile.cFileName << std::endl;
-                                    }
-                                }
+                                // extract and write dbc file to disk
+                                extractDBCFile(hDBCNextFile, hDiskFile, pFile);
                             }
                             else
-                                std::cout << "Failed to create disk file HANDLE:  " << pFile.cFileName << std::endl;
+                                std::cout << "Failed to create the disk file HANDLE:  " << pFile.cFileName << std::endl;
                         }
                         else
-                            std::cout << "Failed to open archive file:  " << pFile.cFileName << std::endl;
+                            std::cout << "Failed to open the archive file:  " << pFile.cFileName << std::endl;
                     }
                 }
                 else
-                    std::cout << "Failed to create disk file HANDLE:  " << pFile.cFileName << std::endl;
+                    std::cout << "Failed to create the disk file HANDLE:  " << pFile.cFileName << std::endl;
             }
             else
-                std::cout << "Failed to open archived file:  " << pFile.cFileName << std::endl;
+                std::cout << "Failed to open the archived file:  " << pFile.cFileName << std::endl;
         }
         else
             std::cout << "PROBLEM! The dbc.MPQ archive is empty!!! " << std::endl;
@@ -123,6 +122,8 @@ void processTheDBCMPQFile()
         CloseHandle(hDBCFirstFile);
     if (hDBCNextFile != NULL)
         CloseHandle(hDBCNextFile);
+    if (hDiskFile != NULL)
+        CloseHandle(hDiskFile);
 }
 
 int main(int argc, char* arg[])
@@ -141,7 +142,7 @@ int main(int argc, char* arg[])
 
     }
     else // failed to locate the WoW executable
-        std::cout << "WoW exe file could not be found!!!" << std::endl << std::endl;
+        std::cout << "The WoW exe file could not be found!!!" << std::endl << std::endl;
 
     std::cout << std::endl << "ALL DONE!!!" << std::endl << std::endl;
 
